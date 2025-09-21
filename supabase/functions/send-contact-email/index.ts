21e041fc795a4174e57e4d1600b68a6b8a7e45e3
Kevy Oliveira <165095995+KevyCodaPouco@users.noreply.github.com>
2025-09-21 02:20:55 -0300

    Update index.ts

--- supabase/functions/send-contact-email/index.ts
+++ supabase/functions/send-contact-email/index.ts
@@ -1,26 +1,22 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import nodemailer from "nodemailer";
-
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type'
 };
-
-serve(async (req) => {
+serve(async (req)=>{
   if (req.method === 'OPTIONS') {
-    return new Response('ok', { headers: corsHeaders });
+    return new Response('ok', {
+      headers: corsHeaders
+    });
   }
-
   try {
     const GMAIL_USER = Deno.env.get('GMAIL_USER');
     const GMAIL_APP_PASSWORD = Deno.env.get('GMAIL_APP_PASSWORD');
-
     if (!GMAIL_USER || !GMAIL_APP_PASSWORD) {
       throw new Error('As variáveis de ambiente do Gmail não foram configuradas.');
     }
-
     const { nome, empresa, email, mensagem } = await req.json();
-
     // @ts-ignore
     const transporter = nodemailer.createTransport({
       hostname: "smtp.gmail.com",
@@ -28,10 +24,9 @@ serve(async (req) => {
       secure: false,
       auth: {
         user: GMAIL_USER,
-        pass: GMAIL_APP_PASSWORD,
-      },
+        pass: GMAIL_APP_PASSWORD
+      }
     });
-
     const mailOptions = {
       from: `"${nome} - Formulário do Site" <${GMAIL_USER}>`,
       to: 'k3vy0liveira@gmail.com',
@@ -42,19 +37,24 @@ serve(async (req) => {
              <p><strong>Mensagem:</strong> ${mensagem}</p>`,
       replyTo: email
     };
-
     // @ts-ignore
     const info = await transporter.sendMail(mailOptions);
-
     return new Response(JSON.stringify(info), {
       status: 200,
-      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      headers: {
+        ...corsHeaders,
+        'Content-Type': 'application/json'
+      }
     });
-
   } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
+    return new Response(JSON.stringify({
+      error: error.message
+    }), {
       status: 500,
-      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      headers: {
+        ...corsHeaders,
+        'Content-Type': 'application/json'
+      }
     });
   }
 });
