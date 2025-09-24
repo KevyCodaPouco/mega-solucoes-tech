1fdbd2714ad1347d59e4faf0194aaa8822d66873
Kevy Oliveira <165095995+KevyCodaPouco@users.noreply.github.com>
2025-09-23 23:29:09 -0300

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -43,7 +43,7 @@ const Navigation = () => {
             ))}
             <div className="flex items-center gap-3">
               <Button variant="outline" size="sm" asChild className="text-primary-foreground border-primary-foreground hover:bg-primary-foreground hover:text-primary">
-                <a href="https://18.231.1.78:3000" target="_blank" rel="noopener noreferrer">
+                <a href="https://grafana.megasoluções.com/login target="_blank" rel="noopener noreferrer">
                   Área do Cliente
                 </a>
               </Button>
